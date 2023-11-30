a995b40fcf9f43ccb8f4610e4336c5a6d51c6912
EmmanuelDP <emmanueldiazp179@gmail.com>
2023-11-29 23:01:34 -0600

    Implementacion del listado y la card en library.dart

--- lib/Screens/library.dart
+++ lib/Screens/library.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_auth/Screens/Sup.dart';
-import 'package:flutter_auth/Screens/bank.dart';
-import 'package:flutter_auth/Screens/hosp.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 import 'package:http/http.dart' as http;
@@ -26,6 +23,41 @@ class _LibraryPageState extends State<LibraryPage> {
   Set<Marker> _markers = {};
   Set<Polyline> _polylines = {};
   LatLng _nearestLibrary = LatLng(0.0, 0.0);
+  TextEditingController _searchController = TextEditingController();
+  bool _showLibraryInfo = false;
+  String _selectedLibraryName = '';
+  String _selectedLibraryAddress = '';
+  String _selectedLibraryPhone = '';
+  String _selectedLibraryHours = '';
+  List<dynamic> _allLibraries = [];
+
+  void _showRouteOnMap(LatLng destination) async {
+  try {
+    await _getDirections(destination);
+    mapController.animateCamera(
+      CameraUpdate.newLatLngBounds(_boundsFromLatLngList([_currentLocation, destination]), 100),
+    );
+  } catch (e) {
+    print("Error al mostrar la ruta en el mapa: $e");
+  }
+}
+
+LatLngBounds _boundsFromLatLngList(List<LatLng> list) {
+  double minLat = list[0].latitude;
+  double maxLat = list[0].latitude;
+  double minLng = list[0].longitude;
+  double maxLng = list[0].longitude;
+
+  for (LatLng latLng in list) {
+    if (latLng.latitude < minLat) minLat = latLng.latitude;
+    if (latLng.latitude > maxLat) maxLat = latLng.latitude;
+    if (latLng.longitude < minLng) minLng = latLng.longitude;
+    if (latLng.longitude > maxLng) maxLng = latLng.longitude;
+  }
+
+  return LatLngBounds(northeast: LatLng(maxLat, maxLng), southwest: LatLng(minLat, minLng));
+}
+
 
   @override
   void initState() {
@@ -67,13 +99,19 @@ class _LibraryPageState extends State<LibraryPage> {
     }
   }
 
-  Future<void> _searchNearbyLibraries() async {
+  Future<void> _searchNearbyLibraries({String? keyword}) async {
     final apiKey = 'AIzaSyDnVASaBzWWIx0ZaO5E5legQLNGrqMIztk';
     final radius = 2500;
     final type = 'library';
 
-    final url =
-        'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${_currentLocation.latitude},${_currentLocation.longitude}&radius=$radius&type=$type&key=$apiKey';
+    String url;
+    if (keyword != null && keyword.isNotEmpty) {
+      url =
+          'https://maps.googleapis.com/maps/api/place/textsearch/json?query=$keyword&location=${_currentLocation.latitude},${_currentLocation.longitude}&radius=$radius&type=$type&key=$apiKey';
+    } else {
+      url =
+          'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${_currentLocation.latitude},${_currentLocation.longitude}&radius=$radius&type=$type&key=$apiKey';
+    }
 
     final response = await http.get(Uri.parse(url));
 
@@ -103,8 +141,14 @@ class _LibraryPageState extends State<LibraryPage> {
             infoWindow: InfoWindow(
               title: name,
             ),
+            onTap: () {
+              _showlibraryInfo(name);
+              _showRouteOnMap(libraryLocation);
+            },
           ),
         );
+
+        _allLibraries.add(library);
       });
 
       _getDirections(_nearestLibrary);
@@ -175,7 +219,7 @@ class _LibraryPageState extends State<LibraryPage> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        key: _scaffoldKey, // Agrega esta línea
+        key: _scaffoldKey,
         appBar: AppBar(
           title: const Text('Librerias'),
           backgroundColor: const Color.fromARGB(255, 56, 139, 142),
@@ -185,9 +229,35 @@ class _LibraryPageState extends State<LibraryPage> {
               _scaffoldKey.currentState!.openDrawer();
             },
           ),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                _searchNearbyLibraries(keyword: _searchController.text);
+              },
+            ),
+          ],
         ),
         body: Column(
           children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: _searchController,
+                decoration: InputDecoration(
+                  labelText: 'Buscar librerías',
+                  suffixIcon: IconButton(
+                    icon: Icon(Icons.clear),
+                    onPressed: () {
+                      _searchController.clear();
+                    },
+                  ),
+                ),
+                onSubmitted: (value) {
+                  _searchNearbyLibraries(keyword: value);
+                },
+              ),
+            ),
             Expanded(
               child: GoogleMap(
                 onMapCreated: _onMapCreated,
@@ -199,6 +269,14 @@ class _LibraryPageState extends State<LibraryPage> {
                 polylines: _polylines,
               ),
             ),
+            ElevatedButton(
+              onPressed: () {
+                _showLibraryList();
+              },
+              child: Text('Mostrar Librerías'),
+            ),
+            if (_showLibraryInfo)
+              _buildLibraryInfoCard()
           ],
         ),
         drawer: Drawer(
@@ -211,43 +289,113 @@ class _LibraryPageState extends State<LibraryPage> {
                 ),
                 child: Text('Menú'),
               ),
-               ListTile(
+              ListTile(
                 title: Text('Supermercados'),
                 onTap: () {
-                  Navigator.pop(context); // Cierra el drawer
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => HospPage()),
-                  );
+                  Navigator.pop(context);
+                  // Agrega la navegación o acciones adicionales según tu necesidad
                 },
               ),
               ListTile(
                 title: Text('Hospitales'),
                 onTap: () {
-                  // Acciones al hacer clic en la opción 2
-                  Navigator.pop(context); // Cierra el drawer
-                   Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => SupPage()),
-                  );
+                  Navigator.pop(context);
+                  // Agrega la navegación o acciones adicionales según tu necesidad
                 },
               ),
-                ListTile(
+              ListTile(
                 title: Text('Bancos'),
                 onTap: () {
-                  // Acciones al hacer clic en la opción 2
-                  Navigator.pop(context); // Cierra el drawer
-                   Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => BankPage()),
-                  );
+                  Navigator.pop(context);
+                  // Agrega la navegación o acciones adicionales según tu necesidad
                 },
               ),
-              // Agrega más opciones según tus necesidades
             ],
           ),
         ),
       ),
     );
+  }
+
+  void _showLibraryList() {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return _buildLibraryList();
+      },
+    );
+  }
+
+  Widget _buildLibraryInfoCard() {
+    return Card(
+      margin: const EdgeInsets.all(8.0),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              '$_selectedLibraryName',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 8.0),
+            Text('Dirección: $_selectedLibraryAddress'),
+            SizedBox(height: 8.0),
+            Text('Teléfono: $_selectedLibraryPhone'),
+            SizedBox(height: 8.0),
+            Text('Horario: $_selectedLibraryHours'),
+            SizedBox(height: 8.0),
+            ElevatedButton(
+              onPressed: _hideLibraryInfo,
+              child: Text('Cerrar'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildLibraryList() {
+    return ListView.builder(
+      itemCount: _allLibraries.length,
+      itemBuilder: (context, index) {
+        final library = _allLibraries[index];
+        return ListTile(
+          title: Text(library['name']),
+          subtitle: Text(library['formatted_address'] ?? 'Dirección no disponible'),
+          onTap: () {
+            Navigator.pop(context);
+            _showlibraryInfo(library['name']);
+            _showRouteOnMap(LatLng(
+              library['geometry']['location']['lat'],
+              library['geometry']['location']['lng'],
+            ));
+          },
+        );
+      },
+    );
+  }
+
+  void _showlibraryInfo(String libraryName) {
+    final selectedLibrary =
+        _allLibraries.firstWhere((library) => library['name'] == libraryName);
+    _selectedLibraryName = selectedLibrary['name'];
+    _selectedLibraryAddress = selectedLibrary['formatted_address'] ?? 'Dirección no disponible';
+    _selectedLibraryPhone = selectedLibrary['formatted_phone_number'] ?? 'Teléfono no disponible';
+    _selectedLibraryHours = selectedLibrary['opening_hours'] != null &&
+            selectedLibrary['opening_hours']['weekday_text'] != null
+        ? selectedLibrary['opening_hours']['weekday_text'][0]
+        : 'Horario no disponible';
+
+    _showLibraryInfo = true;
+    setState(() {});
+  }
+
+  void _hideLibraryInfo() {
+    _showLibraryInfo = false;
+    setState(() {});
   }
 }
